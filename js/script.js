54c0bf48f77051f99c2282f97469d00f2d4fabc3
Rolandas Macius <rolandas.macius@gmail.com>
2024-05-15 10:03:13 +0300

    Pataisymas

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-export const gridBox = document.querySelectorAll('.gridBox');
-export const points = document.querySelector('#points');
+const gridBox = document.querySelectorAll('.gridBox');
+const points = document.querySelector('#points');
 
 
 createRandomBox()
-export function createRandomBox(){
+function createRandomBox(){
     let randomInt = Math.floor(Math.random() * gridBox.length);
     setTimeout(()=>{
         gridBox[randomInt].style.backgroundColor = "red";
@@ -11,18 +11,13 @@ export function createRandomBox(){
         setTimeout(()=>{
             gridBox[randomInt].style.backgroundColor = "white";
             gridBox[randomInt].removeEventListener('click', getPoint)
-
             createRandomBox()
         },500)
     },1000)
-
-
-
-
-    console.log(randomInt)
+    // console.log(randomInt)
 }
 
-export function getPoint(){
+function getPoint(){
     points.textContent = Number(points.textContent) +1;
     // alert('You get 1 point')
 }
